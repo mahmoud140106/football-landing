06fef329a1cf9cbaa6ff4d6b2a6cf79f4f218c06
Eng_3bdo_7amed <3bdo.7amed.com@gmail.com>
2025-01-29 04:54:52 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     return (
         <footer className="mt-10 bg-gradient-to-r bg-[#27AE60]   py-10 px-10 font-sans tracking-wide">
             <div className="max-w-2xl mx-auto text-center">
-                <h2 className="text-4xl font-semibold text-white">Seasoned. Nimble. Remote.</h2>
+                <h2 className="text-4xl font-semibold text-white">Live Footballia</h2>
                 <p className="text-sm mt-8 text-gray-300">We’re a diverse and passionate team that takes ownership of your design and empower you to execute the roadmap. We stay light on our feet and truly enjoy delivering great work.
                 </p>
             </div>
@@ -13,8 +13,10 @@ export default function Footer() {
             <hr className="my-10 mx-auto max-w-5xl border-gray-500" />
 
 
-            <div className=" max-md:flex-col gap-4">
-                <p className="text-sm text-center text-gray-300">Copyright © 2023. All rights reserved</p>
+            <div className=" max-md:flex-col gap-4 space-y-2">
+                <p className="text-sm text-center text-gray-300">Copyright © 2025 - Live Footballia. All rights reserved</p>
+                <p className="text-sm text-center text-gray-300">Made by <a href="https://github.com/Ahmed-AbdElhameed" className="text-md font-semibold text-gray-100 hover:text-white transition-colors"> &#34; DRAM Code &#34; </a>Team</p>
+
 
             </div>
         </footer>
