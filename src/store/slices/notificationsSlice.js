57a7614f6cb38d8cac8b1e98c93e939fc0658203
mahmoud140106 <amer140106@gmail.com>
2025-02-22 21:29:28 +0200

    notifications

--- src/store/slices/notificationsSlice.js
+++ src/store/slices/notificationsSlice.js
@@ -26,26 +26,9 @@ export const saveSubscription = createAsyncThunk(
   }
 );
 
-export const getSubscriptions = createAsyncThunk(
-  "notifications/getSubscriptions",
-  async (_, { rejectWithValue }) => {
-    try {
-      const response = await api.get("api/v2/notifications/subscriptions", {
-        params: { lang: "en" },
-      });
-      return response.data.data;
-    } catch (error) {
-      return rejectWithValue(
-        error.response?.data?.error?.message || "Something went wrong"
-      );
-    }
-  }
-);
-
 const notificationsSlice = createSlice({
   name: "notifications",
   initialState: {
-    subscriptions: [],
     isLoading: false,
     isError: false,
     errorMessage: "",
@@ -64,25 +47,8 @@ const notificationsSlice = createSlice({
         state.isLoading = false;
         state.isError = false;
         state.errorMessage = "";
-        state.subscriptions.push(action.payload);
       })
       .addCase(saveSubscription.rejected, (state, action) => {
-        state.isLoading = false;
-        state.isError = true;
-        state.errorMessage = action.payload;
-      })
-      .addCase(getSubscriptions.pending, (state) => {
-        state.isLoading = true;
-        state.isError = false;
-        state.errorMessage = "";
-      })
-      .addCase(getSubscriptions.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.isError = false;
-        state.errorMessage = "";
-        state.subscriptions = action.payload || [];
-      })
-      .addCase(getSubscriptions.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.errorMessage = action.payload;
